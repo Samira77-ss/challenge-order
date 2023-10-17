fac5dc3a4dcb3c5b0772b7f6dd5832878a425351
Samira77-ss <samiraseyfii0709@gmail.com>
2023-10-17 11:23:09 +0330

    Added initial structure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <img src="https://order-summary-component-mavix.netlify.app/images/illustration-hero.svg" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
